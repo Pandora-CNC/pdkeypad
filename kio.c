8876b8549371ccc05d58194e259e27548c60e2b0
Benedikt Müssig <benedikt.muessig@gmail.com>
2017-04-21 10:36:15 +0200

    Implemented the entire I/O

---
+++ kio.c
@@ -0,0 +1,105 @@
+/*
+  pdkeypad
+  An open-source reimplementation of the Pandora keypad driver
+  (c) 2017 0xbmuessig, bmuessig.eu
+*/
+
+#include "kio.h"
+
+void kio_init(void)
+{
+  // Configure the DIO line to: Output, HIGH
+  w55fa93_gpio_set_output(DIO_GRP, DIO_PIN);
+  w55fa93_gpio_set(DIO_GRP, DIO_PIN, HIGH);
+
+  // Configure the CLK line to: Output, LOW
+  w55fa93_gpio_set_output(CLK_GRP, CLK_PIN);
+  w55fa93_gpio_set(CLK_GRP, CLK_PIN, LOW);
+
+  // Configure the STB line to: Output, HIGH
+  w55fa93_gpio_set_output(STB_GRP, STB_PIN);
+  w55fa93_gpio_set(STB_GRP, STB_PIN, HIGH);
+
+  // Configure the unknown pins to: Input
+  w55fa93_gpio_set_input(UK0_GRP, UK0_PIN);
+  w55fa93_gpio_set_input(UK1_GRP, UK1_PIN);
+}
+
+void kio_begin_transaction(void)
+{
+  // Set the clock low (since data is latched on the rising edge)
+  w55fa93_gpio_set(CLK_GRP, CLK_PIN, LOW);
+  // Enable the serial interface
+  w55fa93_gpio_set(STB_GRP, STB_PIN, LOW);
+  // Sleep until the serial interface is ready
+  usleep(SIF_TTI);
+}
+
+void kio_end_transaction(void)
+{
+  // Disable the serial interface
+  w55fa93_gpio_set(STB_GRP, STB_PIN, HIGH);
+  // Set the clock low (since data is latched on the rising edge)
+  w55fa93_gpio_set(CLK_GRP, CLK_PIN, LOW);
+}
+
+void kio_write(unsigned char data)
+{
+  int bit;
+
+  // Set the DIO to an output
+  w55fa93_gpio_set_output(DIO_GRP, DIO_PIN);
+
+  for(bit = 0; bit < 8; bit++) {
+    // Set the clock to low
+    w55fa93_gpio_set(CLK_GRP, CLK_PIN, LOW);
+    // Put the correct bit LSB-first onto DIO
+    w55fa93_gpio_set(DIO_GRP, DIO_PIN, (data >> bit) & 1);
+    // Wait for the pins to settle
+    usleep(SIF_CLP);
+    // Rise the clock
+    w55fa93_gpio_set(CLK_GRP, CLK_PIN, HIGH);
+    // Wait for the second half of the clock period
+    usleep(SIF_CLP);
+  }
+}
+
+unsigned char kio_read(void)
+{
+  int bit;
+  unsigned char word = 0;
+
+  // Set the DIO to an input
+  w55fa93_gpio_set_input(DIO_GRP, DIO_PIN);
+
+  for(bit = 0; bit < 8; bit++) {
+    // Set the clock to low
+    w55fa93_gpio_set(CLK_GRP, CLK_PIN, LOW);
+    // Wait for the pins to settle
+    usleep(SIF_CLP);
+    // Read the current bit on DIO and store it
+    if(w55fa93_gpio_get(DIO_GRP, DIO_PIN))
+      word |= (1 << bit);
+    // Rise the clock again
+    w55fa93_gpio_set(CLK_GRP, CLK_PIN, HIGH);
+  }
+}
+
+unsigned int kio_keys(void)
+{
+  int word, buffer = 0;
+
+  // Start the transfer
+  kio_begin_transaction();
+  // Transmit the instruction byte
+  kio_write(INS_RKR);
+  // Wait until the device is surely ready
+  usleep(SIF_TTD);
+  // Read 4 bytes
+  for(word = 0; word < 4; word++)
+    buffer = (buffer << 8) | (kio_read() & 0xFF);
+  // End the transfer
+  kio_end_transaction();
+  // Return the buffer
+  return buffer;
+}
---
+++ kio.h
@@ -0,0 +1,50 @@
+/*
+  pdkeypad
+  An open-source reimplementation of the Pandora keypad driver
+  (c) 2017 0xbmuessig, bmuessig.eu
+*/
+
+#ifndef KIO_H
+#define KIO_H
+
+#include <mach/w55fa93_gpio.h>
+#include <unistd.h>
+
+// For readability, define HIGH and LOW constants
+#define HIGH 1
+#define LOW 0
+
+// Read keys instruction
+#define INS_RKR 0x42
+
+// Duration in usecs until the serial interface is ready
+#define SIF_TTI 1
+// Duration in usecs for the clock pulse
+#define SIF_CLP 1
+// Duration in usecs for the data to appear
+#define SIF_TTD 1
+
+// DIO (digital I/O) I/O definition
+#define DIO_GRP GPIO_GROUP_D
+#define DIO_PIN 0
+// CLK (clock) I/O definition
+#define CLK_GRP GPIO_GROUP_D
+#define CLK_PIN 1
+// STB (chip select) I/O definition
+#define STB_GRP GPIO_GROUP_D
+#define STB_PIN 2
+
+// TODO: Find out, what UK0 and UK1 do
+#define UK0_GRP GPIO_GROUP_D
+#define UK0_PIN 12
+#define UK1_GRP GPIO_GROUP_D
+#define UK1_PIN 13
+
+void kio_init(void);
+void kio_begin_transaction(void);
+void kio_end_transaction(void);
+void kio_write(unsigned char data);
+unsigned char kio_read(void);
+unsigned int kio_keys(void);
+
+#endif
