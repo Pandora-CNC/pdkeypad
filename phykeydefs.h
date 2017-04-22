eb9668878501872a12679a4c53420f2cf5f02301
Benedikt Müssig <benedikt.muessig@gmail.com>
2017-04-22 11:52:08 +0200

    Renamed the physical key definitions

---
+++ phykeydefs.h
@@ -0,0 +1,33 @@
+/*
+  pdkeypad
+  An open-source reimplementation of the Pandora keypad driver
+  (c) 2017 0xbmuessig, bmuessig.eu
+
+  phykeydefs.h: Physical key definitions for the DDCSV1.1
+*/
+
+#ifndef PHYKEYDEFS_H
+#define PHYKEYDEFS_H
+
+#include "kio.h"
+
+// TODO: Use a DMM to trace out which pin goes where
+#define PHYK_FRO       KEY(0,0)
+#define PHYK_SELECT    KEY(0,0)
+#define PHYK_2ND       KEY(0,0)
+#define PHYK_MODIFY    KEY(0,0)
+#define PHYK_MODE      KEY(0,0)
+#define PHYK_CANCEL    KEY(0,0)
+#define PHYK_ENTER     KEY(0,0)
+#define PHYK_UP        KEY(0,0)
+#define PHYK_DOWN      KEY(0,0)
+#define PHYK_LEFT      KEY(0,0)
+#define PHYK_RIGHT     KEY(0,0)
+#define PHYK_START     KEY(0,0)
+#define PHYK_PAUSE     KEY(0,0)
+#define PHYK_RST       KEY(0,0)
+#define PHYK_PAGE      KEY(0,0)
+#define PHYK_TOOLPATH  KEY(0,0)
+#define PHYK_SPINDLE   KEY(0,0)
+
+#endif
