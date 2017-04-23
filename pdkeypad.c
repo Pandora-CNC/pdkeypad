bb8f383100cbc466d07cecc1d21611b296698eda
Benedikt Müssig <benedikt.muessig@gmail.com>
2017-04-23 09:20:22 +0200

    Added uinput init

--- pdkeypad.c
+++ pdkeypad.c
@@ -1,7 +1,52 @@
 #include "pdkeypad.h"
 
-int main(unsigned int argc, char* argv[]) {
-  int fd = open("/dev/input/uinput", O_WRONLY | O_NONBLOCK);
+int uinput_fd;
+
+int main(unsigned int argc, char* argv[])
+{
+  if(!init(UINPUT_DEVICE))
 
   return 0;
+}
+
+int init(char* uinput_dev)
+{
+  if(!(uinput_fd = open(uinput_fd, O_WRONLY | O_NONBLOCK))) {
+
+  }
+
+  int err = 0;
+
+  // Enable press and release synchronisation events
+  err |= (ioctl(uinput_fd, UI_SET_EVBIT, EV_KEY) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_EVBIT, EV_SYN) < 0);
+
+  if(err) {
+    fprintf(stderr, "ERR: Registering uinput events failed!\n");
+    return false;
+  }
+
+  // Enable events for all keys we care about
+  err = (ioctl(uinput_fd, UI_SET_KEYBIT, K_FRO) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_SELECT) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_2ND) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_MODIFY) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_MODE) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_CANCEL) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_ENTER) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_UP) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_DOWN) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_LEFT) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_RIGHT) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_START) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_PAUSE) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_RST) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_PAGE) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_TOOLPATH) < 0);
+  err |= (ioctl(uinput_fd, UI_SET_KEYBIT, K_SPINDLE) < 0);
+
+  if(err) {
+    fprintf(stderr, "ERR: Registering uinput keys failed!\n");
+    return false;
+  }
 }
--- pdkeypad.h
+++ pdkeypad.h
@@ -4,5 +4,13 @@
 #include <linux/input.h>
 #include <linux/uinput.h>
 
+#include "kio.h"
+#include "phykeydefs.h"
+#include "pdkeydefs.h"
+
+#define UINPUT_DEVICE "/dev/input/uinput"
+
 int main(unsigned int argc, char* argv[]);
+int init(char uinput_dev);
+
 #endif
